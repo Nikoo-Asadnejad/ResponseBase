bbdfa499208e6c5b9dd752927605f32d85c3b2de
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2023-04-20 12:58:38 +0330

    Feat : Add httpStatusExtension methods

---
+++ ResponseHelpers/Tools/HttpStatusExtensions.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponseHelper.Tools;
+public static class HttpStatusExtensions
+{
+  public static bool IsSuccessfulll(this HttpStatusCode httpStatusCode)
+  => (int)httpStatusCode >= 200 && (int)httpStatusCode < 300;
+  public static bool IsRedirected(this HttpStatusCode httpStatusCode)
+  => (int)httpStatusCode >= 300 && (int)httpStatusCode < 400;
+  public static bool IsError(this HttpStatusCode httpStatusCode)
+  => (int) httpStatusCode >= 400 && (int) httpStatusCode < 500;
+  public static bool IsServerError(this HttpStatusCode httpStatusCode)
+  => (int)httpStatusCode >= 500 && (int)httpStatusCode < 600;
+
+  public static bool IsSuccessfulll(this int httpStatusCode)
+ => (int)httpStatusCode >= 200 && (int)httpStatusCode < 300;
+  public static bool IsRedirected(this int httpStatusCode)
+  => (int)httpStatusCode >= 300 && (int)httpStatusCode < 400;
+  public static bool IsError(this int httpStatusCode)
+  => (int)httpStatusCode >= 400 && (int)httpStatusCode < 500;
+  public static bool IsServerError(this int httpStatusCode)
+  => (int)httpStatusCode >= 500 && (int)httpStatusCode < 600;
+}
+
