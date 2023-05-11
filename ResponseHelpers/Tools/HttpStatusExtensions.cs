b80db3e46a37fd1899535f6c8346335c47bc1f2d
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2023-05-11 10:50:32 +0330

    feat(httpStatusExtensions) : add IsUnAuthorized extension method

--- ResponseHelpers/Tools/HttpStatusExtensions.cs
+++ ResponseHelpers/Tools/HttpStatusExtensions.cs
@@ -16,14 +16,18 @@ public static class HttpStatusExtensions
   => (int) httpStatusCode >= 400 && (int) httpStatusCode < 500;
   public static bool IsServerError(this HttpStatusCode httpStatusCode)
   => (int)httpStatusCode >= 500 && (int)httpStatusCode < 600;
+  public static bool IsUnAuthorized(this HttpStatusCode httpStatusCode)
+      => (int)httpStatusCode is 401 || (int)httpStatusCode is 403;
 
   public static bool IsSuccessfulll(this int httpStatusCode)
- => (int)httpStatusCode >= 200 && (int)httpStatusCode < 300;
+ => httpStatusCode >= 200 && httpStatusCode < 300;
   public static bool IsRedirected(this int httpStatusCode)
-  => (int)httpStatusCode >= 300 && (int)httpStatusCode < 400;
+  => httpStatusCode >= 300 && httpStatusCode < 400;
   public static bool IsError(this int httpStatusCode)
-  => (int)httpStatusCode >= 400 && (int)httpStatusCode < 500;
+  => httpStatusCode >= 400 && httpStatusCode < 500;
   public static bool IsServerError(this int httpStatusCode)
-  => (int)httpStatusCode >= 500 && (int)httpStatusCode < 600;
+  => httpStatusCode >= 500 && httpStatusCode < 600;
+  public static bool IsUnAuthorized(this int httpStatusCode)
+      => httpStatusCode is 401 || httpStatusCode is 403;
 }
 
